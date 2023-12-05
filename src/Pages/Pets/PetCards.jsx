218aa74ebd7e38ab4986ca84078b68d5722980de
Jahirul83 <jahirulislamcse83.com>
2023-12-06 00:52:58 +0600

    resize card image

--- src/Pages/Pets/PetCards.jsx
+++ src/Pages/Pets/PetCards.jsx
@@ -7,7 +7,7 @@ const PetCards = ({ pet }) => {
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
-                    <img src={image} alt="Shoes" className="rounded-xl" />
+                    <img src={image} alt="Shoes" className="rounded-xl w-60 h-60" />
                 </figure>
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{name}</h2>
