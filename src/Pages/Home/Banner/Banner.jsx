8a2c71f180a79db94cf3573e3b6e199f461daa8c
Jahirul83 <jahirulislamcse83@gmail.com>
2023-12-06 00:23:59 +0600

    banner edit

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -11,8 +11,8 @@ const Banner = () => {
                     <img src={img1} className="w-full h-auto" />
                     <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                         <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h3 className='text-4xl font-bold'>Welcome to Our Library</h3>
-                            <p>Explore a World of Knowledge and Imagination</p>
+                            <h3 className='text-4xl font-bold'>Welcome to Our Pet Adoption Platform</h3>
+                            <p>Welcome to pet-shop , where love finds a home. Discover a world of adorable pets waiting for a loving family. Browse through our diverse selection of cats, dogs, and more. Your perfect companion is just a click away!</p>
                         </div>
                     </div>
                 </div>
@@ -20,8 +20,8 @@ const Banner = () => {
                     <img src={img2} className="w-full h-auto" />
                     <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                         <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h3 className='text-4xl font-bold'>Discover Featured Collections</h3>
-                            <p>From Classics to Contemporary Reads</p>
+                            <h3 className='text-4xl font-bold'>Why Adopt a Pet?</h3>
+                            <p>: When you adopt a pet, your not just gaining a companion; your saving a life. Explore the rewards of adoption, from loyal friendships to unforgettable moments. Every adoption contributes to a brighter future for animals in need. Join us in making a difference today!</p>
                         </div>
                     </div>
                 </div>
@@ -29,8 +29,8 @@ const Banner = () => {
                     <img src={img3} className="w-full h-auto" />
                     <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                         <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h3 className='text-4xl font-bold'>Join Our Exciting Events</h3>
-                            <p>Book Clubs, Author Talks, and More!</p>
+                            <h3 className='text-4xl font-bold'> Meet Our Rescued Stars</h3>
+                            <p>Get inspired by the incredible stories of pets who have overcome adversity. Meet our rescued stars—pets with tales of resilience and the power of second chances. Their stories remind us that every adoption creates a new chapter filled with love and joy.</p>
                         </div>
                     </div>
                 </div>
@@ -38,8 +38,8 @@ const Banner = () => {
                     <img src={img4} className="w-full h-auto" />
                     <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                         <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h3 className='text-4xl font-bold'>Access Our Digital Resources</h3>
-                            <p>eBooks, Audiobooks, and Online Learning</p>
+                            <h3 className='text-4xl font-bold'>How It Works</h3>
+                            <p>Ready to welcome a furry friend? Our adoption process is easy and stress-free. Explore our profiles, connect with our dedicated team, and let the journey to a forever home begin. Your new best friend is eagerly waiting for you.</p>
                         </div>
                     </div>
                 </div>
