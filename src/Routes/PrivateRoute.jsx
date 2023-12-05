5d88296537055598264e43b13452308aec0d53ab
Jahirul83 <jahirulislamcse83.com>
2023-12-05 19:47:09 +0600

    private route errorpage

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,21 @@
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+    const { user,loading } = useContext(AuthContext);
+    const location = useLocation();
+    if (loading) {
+        return <progress className="progress w-56" value="100" max="100"></progress>;
+    }
+    if (user) {
+        return children;
+    }
+    return <Navigate state={location.pathname} to='/login'></Navigate>
 };
 
 export default PrivateRoute;
+PrivateRoute.propTypes = {
+    children: PropTypes.node
+}
